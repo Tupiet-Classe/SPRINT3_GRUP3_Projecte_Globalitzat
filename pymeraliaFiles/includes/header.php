b6799917d18f6b6d78a4dcd680cde05896cf85fc
Sergi-Fornos-Gonzalez <sergifornosinstitut@gmail.com>
2022-11-28 17:39:10 +0100

    creats arxius header i footer per includes

---
+++ pymeraliaFiles/includes/header.php
@@ -0,0 +1,73 @@
+<header class="sticky-top">
+      <div class="navbar navbar-expand-sm p-0" id="header-logo">
+          <div class="container-fluid d-flex flex-row justify-content-between navbar-nav ">
+              <div class="p-2" id="logo">
+                  <li class="nav-item"><a class="nav-link" href="#"><img src="../images/logo_pymeshield.png"
+                              alt="Logo" class="d-inline-block align-text-middle">
+                          pymeshield</a></li>
+              </div>
+
+              <!--Ruptura del responsive en 576px a 575px-->
+              <div class="p-2">
+                  <div class="container" id="navbarScroll">
+                      <ul class="navbar-nav me-auto my-2" style="--bs-scroll-height: 100px;">
+                          <li class="nav-item dropdown">
+                              <a class="nav-link dropdown-toggle" id="menu-dropdown" href="#" role="button"
+                                  data-bs-toggle="dropdown" aria-expanded="false">
+                                  <i class="fa-solid fa-user"></i>
+                              </a>
+                              <ul class="dropdown-menu" id="menu-user">
+                                  <li><a class="dropdown-item" href="#"><i class="fa-solid fa-address-card"></i>Editar
+                                          Perfil</a></li>
+                                  <li><a class="dropdown-item" href="#"><i class="fa-solid fa-language"></i>Idioma</a>
+                                  </li>
+                                  <li><a class="dropdown-item" href="#"><i class="fa-solid fa-palette"></i>Tema</a>
+                                  </li>
+                                  <li><a class="dropdown-item" href="#"><i
+                                              class="fa-solid fa-right-from-bracket"></i>Cerrar Sesión</a></li>
+                                  <li>
+                                      <hr class="dropdown-divider">
+                                  </li>
+                                  <li><a class="dropdown-item" href="../cliente/index.php"><i
+                                              class="fa-solid fa-shield-halved"></i>Modo Usuario</a></li>
+                              </ul>
+                          </li>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <!--Header Logo-->
+
+
+      <nav class="navbar navbar-expand-lg p-0" id="main-navbar">
+          <div class="container-fluid">
+              <span class="p-2">
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                  </button></span>
+              <div class="collapse navbar-collapse p-0" id="navbarNav">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="index.php"><i
+                                  class="fa-solid fa-house"></i>Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="cursos.php"><i
+                                  class="fa-solid fa-graduation-cap"></i>Listado Cursos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="usuariosCurso.php"><i
+                                    class="fa-solid fa-users"></i>Listado Alumnos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="crearActividad.php"><i 
+                        class="fa-solid fa-circle-plus"></i>Crear Actividad</a></li>
+                    <li class="nav-item"><a class="nav-link" href="editarCurso.php"><i
+                                  class="fa-solid fa-award"></i>Editar Curso</a></li>
+                    <li class="nav-item"><a class="nav-link" href="editarNotaUsuarios.php"><i
+                                  class="fa-solid fa-star"></i>Editar Nota</a>
+                                  
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="emblemasAdmin.php"><i
+                        class="fa-solid fa-certificate"></i>Editar Emblemas</a>
+            </li>
+                  </ul>
+              </div>
+      </nav>
+      <!--Header Menu-->
+
+  </header>
