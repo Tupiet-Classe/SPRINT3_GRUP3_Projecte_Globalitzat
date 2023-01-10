a8ecbe1b7372de526bda85f58264aed41d84b737
Sergi-Fornos-Gonzalez <sergifornosinstitut@gmail.com>
2023-01-10 17:00:34 +0100

    avançat classe deliver

--- pymeraliaFiles/PHP/addDeliver.php
+++ pymeraliaFiles/PHP/addDeliver.php
@@ -1,6 +1,9 @@
 <?php
-include_once '../clases/DeliverClass';
+include_once '../clases/Deliver_Class';
 $deliver_file = $_FILES['nomArchiu'];
 $activity_id = $_POST['idActivitat'];
 $user_id = $_POST['idUsuari'];
+
+$deliver = new Deliver($deliver_file,$activity_id,$user_id);
+$deliver->getFile();
 ?>
