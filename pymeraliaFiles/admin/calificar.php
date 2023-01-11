7c0591f918431ed27d032ef25c32a1462db236df
Sergi-Fornos-Gonzalez <sergifornosinstitut@gmail.com>
2023-01-11 20:06:27 +0100

    by:julia canviar titul

--- pymeraliaFiles/admin/calificar.php
+++ pymeraliaFiles/admin/calificar.php
@@ -37,7 +37,7 @@
           <div class="modal-content">
             <form method="POST" id="apply" action="../../PHP/apply.php">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Insereix el nom de l'usuari per verificar</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Insereix la nota de l'alumne</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">  
